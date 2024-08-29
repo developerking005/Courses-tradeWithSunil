da9503e1fffb9555107aa0fc4495bacde616f2af
developerking005 <160459748+developerking005@users.noreply.github.com>
2024-08-29 13:04:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('scroll-btn').addEventListener('click', function() {
+    document.getElementById('courses').scrollBy({
+        left: 370,
+        behavior: 'smooth'
+    });
+});
